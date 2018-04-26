e2413685de2e449de4a0baad2a03dcfac33dc07a
vaibhavmorye <moryevaibhav93@gamil.com>
2018-04-26 21:37:10 +0530

    first commit

--- src/main/java/com/ngboot/CURD_example_ng_boot/Controller/ProductController.java
+++ src/main/java/com/ngboot/CURD_example_ng_boot/Controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ngboot.CURD_example_ng_boot.model.Product;
 import com.ngboot.CURD_example_ng_boot.repository.ProductRepository;
 
-@Controller
+@CrossOrigin(origins= {"http://localhost:4200"}, maxAge = 4800, allowCredentials = "false")
 @RestController
 public class ProductController {
 
