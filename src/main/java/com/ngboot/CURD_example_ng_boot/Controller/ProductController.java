662cd787056ef62a13a9d8c4f7e6c09966ebaeab
vaibhavmorye <moryevaibhav93@gamil.com>
2018-04-29 18:16:47 +0530

    bug fix

--- src/main/java/com/ngboot/CURD_example_ng_boot/Controller/ProductController.java
+++ src/main/java/com/ngboot/CURD_example_ng_boot/Controller/ProductController.java
@@ -3,7 +3,9 @@ package com.ngboot.CURD_example_ng_boot.Controller;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ngboot.CURD_example_ng_boot.model.Product;
 import com.ngboot.CURD_example_ng_boot.repository.ProductRepository;
 
-@CrossOrigin(origins= {"*"}, maxAge = 4800, allowCredentials = "false")
+@CrossOrigin(origins = { "*" }, maxAge = 4800, allowCredentials = "false")
 @RestController
 public class ProductController {
 
@@ -27,17 +29,18 @@ public class ProductController {
 		return (ArrayList<Product>) productRepository.findAll();
 	}
 
-	@RequestMapping(value = "/addProduct", consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
-	public void setProduct(@RequestBody Product product) {
-
+	@RequestMapping(value = "/addProduct", method = RequestMethod.POST)
+	public ResponseEntity<?> setProduct(@RequestBody Product product) {
+		System.out.println("upp");
 		boolean exist = false;
-
+		System.out.println(product.toString());
 		if (productRepository.equals(product)) {
 			exist = true;
 		} else if (exist == false) {
 			productRepository.save(product);
 
 		}
+		return new ResponseEntity<Product>(product, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/findproduct/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
@@ -45,15 +48,18 @@ public class ProductController {
 		return productRepository.findByproductId(id);
 
 	}
-	
-	@RequestMapping(value = "/deleteproduct/{id}", method = RequestMethod.GET )
-	public void deleteProductById(@PathVariable int id) {
-		 productRepository.delete(id);
+
+	@RequestMapping(value = "/deleteproduct/{id}", method = RequestMethod.GET)
+	public Product deleteProductById(@PathVariable int id) {
+		Product product = productRepository.findByproductId(id);
+		productRepository.delete(id);
+		return product;
+
 	}
-	
-	@RequestMapping(value = "/bulkupload", consumes= MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST )
+
+	@RequestMapping(value = "/bulkupload", consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
 	public void bulkUpload(@RequestBody ArrayList<Product> products) {
-		Iterable< Product> iProducts = products;
+		Iterable<Product> iProducts = products;
 		productRepository.save(iProducts);
 	}
 
